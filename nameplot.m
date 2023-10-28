53d4ebcd5d360fa4c19e984005abd4d2d89e32c3
Haiman Syafiq <20b2114@ubd.edu.bn>
2023-10-28 18:01:25 +0800

    completed name, added nameplot function

---
+++ nameplot.m
@@ -0,0 +1,7 @@
+function nameplot(x, y, xx, yy)
+
+figure(1)
+plot(xx, yy, 'k', 'LineWidth', 1.5)
+hold on
+plot(x, y, 'bo', 'MarkerFaceColor','b')
+
--- names_haiman.m
+++ names_haiman.m
@@ -1,39 +1,68 @@
 %% Definining data points for the name "Haiman"
 
 % Coordinates of cursive H
-hx = [1 2 1.25 0.75 0.25 3.75 3.25 2.5 3.75];
-hy = [4 5 2 0.25 1.25 4.25 4.75 0.25 0.25];
+hx = [1 2 1.25 0.75 0.25 3.75 3.25 2.5 3.8];
+hy = [4 5 2 0.25 1.25 4.25 4.75 1 0.25];
 
 % Coordinates of A
-ax = [4.25 5.22 5.7 5.22 4.25 4.25 5.75 5.75 5.56 7];
-ay = [1.5 2.5 2.4 2.5 1.5 0 0.5 2.5 0.6 0.25];
+ax = [3.8 5.32 5.8 5.32 4.4 4.4 5.85 5.85 5.66 7.1];
+ay = [0.25 2.5 2.4 2.5 1.5 0 0.5 2.5 0.6 0.25];
 
 % Coordinates of I
-ix = [8 8.25 7.75 7.75 9];
-iy = [1.25 2.5 1.25 0.25 0.25];
+ix = [7.1 8.2 7.5 7.7 8.45];
+iy = [0.25 2.5 1.8 0 0.25];
 
-% Combine all axes
-x = [hx ax ix];
-y = [hy ay iy];
+% Coordinates of M
+mx = [8.45 9 9.2 9.05 9 9.1 9.8 10.4 10.6 10.6 10.8 11.4 11.8 12.1 12.5];
+my = [0.25 1.46 2.5 2 0 0.5 2.5 1.5 0 0.5 1.5 2.5 1.5 0 0.25];
 
+% Coordinates of second A
+ax2 = ax + 8.7;
+ay2 = ay;
+
+% Coordinates of N
+nx = [15.8 16.4 16.25 16.2 16.3 16.8 17.5 17.8 18.5];
+ny = [0.25 2.5 2 0 1 2.5 1.5 0 0.25];
 %------------------------------------------------------------------
 
 % Spline interpolation
-[t, tt] = tpar(x);
-xx = spline(t, x, tt);
-yy = spline(t, y, tt);
+[t, tt] = tpar(hx);
+hxx = spline(t, hx, tt);
+hyy = spline(t, hy, tt);
+
+[t, tt] = tpar(ax);
+axx = spline(t, ax, tt);
+ayy = spline(t, ay, tt);
+
+[t, tt] = tpar(ix);
+ixx = spline(t, ix, tt);
+iyy = spline(t, iy, tt);
+
+[t, tt] = tpar(mx);
+mxx = spline(t, mx, tt);
+myy = spline(t, my, tt);
+
+[t, tt] = tpar(ax2);
+axx2 = spline(t, ax2, tt);
+ayy2 = spline(t, ay2, tt);
+
+[t, tt] = tpar(nx);
+nxx = spline(t, nx, tt);
+nyy = spline(t, ny, tt);
+
 
 %------------------------------------------------------------------
 
 % Plot settings
-figure(1)
-plot(xx, yy, 'k', 'LineWidth', 1.5)
-hold on
-plot(x, y, 'bo', 'MarkerFaceColor','b')
-% axis ([-0.5 20 -0.5 6])
+nameplot(hx,hy,hxx,hyy)
+nameplot(ax,ay,axx,ayy)
+nameplot(ix,iy,ixx,iyy)
+nameplot(mx,my,mxx,myy)
+nameplot(ax2,ay2,axx2,ayy2)
+nameplot(nx,ny,nxx,nyy)
 grid on
+axis ([-1 20 -2 6])
 
 xlabel('X')
 ylabel('Y')
-
 hold off
